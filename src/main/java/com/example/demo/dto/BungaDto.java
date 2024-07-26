5c30f0c11ac9de0561fcffe077d287fda7212774
Alfajri <alfajri607@gmail.com>
2024-07-26 17:11:31 +0700

    update

--- src/main/java/com/example/demo/dto/BungaDto.java
+++ src/main/java/com/example/demo/dto/BungaDto.java
@@ -5,11 +5,6 @@ import lombok.Data;
 @Data
 public class BungaDto {
     private Integer tenor;
-    private Integer JumlahPinjaman;
+    private Integer jumlahPinjaman;
     private Integer bunga;
-//    rumus bunga = bunga% * (tenor / 360) * jp
-//            bunga = 10
-//    tenor = 30
-//    jumlah pinjaman = 100 ribu
-//    bunga = % * (tenor/360) * jp
 }
